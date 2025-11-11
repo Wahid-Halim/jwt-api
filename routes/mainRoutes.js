3dbd824776a318f6ca9743bdf38b673a990737bf
Wahid Halim <wahidkeo@gmail.com>
2025-11-11 08:13:07 +0430

    add validation to password and username in controllers

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { login, dashboard } = require("../controllers/mainController");
-const { route } = require("express/lib/router");
 
 router.route("/dashboard").get(dashboard);
-router.route("/login").get(login);
+router.route("/login").post(login);
 
 module.exports = router;
