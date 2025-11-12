4b29a59f8cb8ae20a85e002cad391b2c60f89e86
Wahid Halim <wahidkeo@gmail.com>
2025-11-12 16:38:58 +0430

    add jwt token and verify it

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
@@ -7,15 +8,35 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomAPIError("Please provide username and password", 400);
   }
-  res.send("Fake LOGIN/Register/Sign up");
+
+  // just for demo, in real project this provided by DB
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: `Hello Wahid Halim`,
-    secret: `Here is your authorize data, your lucky number is ${luckyNumber}`,
-  });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello ${decoded.username}`,
+      secret: `Here is your authorize data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access", 401);
+  }
 };
 
 module.exports = {
