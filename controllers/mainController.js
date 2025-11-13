566bbb89d4a3ead8b797b8a95a7c1e2e175df6c1
Wahid Halim <wahidkeo@gmail.com>
2025-11-13 08:28:41 +0430

    Add more cusotme error and use http-status-codes package for stausCode

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -15,28 +15,18 @@ const login = async (req, res) => {
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-  
+
   res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
+  console.log(req.user);
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new CustomAPIError("No token provided", 401);
-  }
-
-  const token = authHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello ${decoded.username}`,
-      secret: `Here is your authorize data, your lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError("Not authorized to access", 401);
-  }
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello ${req.user.username}`,
+    secret: `Here is your authorize data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
